680ef430130e379d1af63ef4987c467d4e622f32
eRkušinko <80844892+eRkusin@users.noreply.github.com>
2025-10-23 11:29:24 +0200

    Add files via upload

---
+++ calcul.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int minimal(const int pole[], const int pocet);
+int maximal(const int pole[], const int pocet);
+int summary(const int pole[], const int pocet);
+double average(const int pole[], const int pocet);
