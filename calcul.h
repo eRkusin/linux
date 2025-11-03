4f32ba0b38129af300a163c26fed44f596a742c7
eRkušinko <80844892+eRkusin@users.noreply.github.com>
2025-11-03 16:56:59 +0100

    Delete calcul.h

--- calcul.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int minimal(const int pole[], const int pocet);
-int maximal(const int pole[], const int pocet);
-int summary(const int pole[], const int pocet);
-double average(const int pole[], const int pocet);
