6a640195a6da9f4f37585adcc3914881879f668f
eRkušinko <80844892+eRkusin@users.noreply.github.com>
2025-11-03 17:14:29 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -1,67 +1,83 @@
 #include "calcul.h"
 
-// minimalna hmotnost – vracia index
+// Funkcia vo vstupnom poli najde prvok s minimalnou hmotnostou
 int minimal(const int pole[], const int pocet)
 {
-    if (pole == 0 || pocet <= 0) return -1;
+    if (pocet <= 0 || pole == 0)
+        return -1;
 
     int i;
-    int minIndex = 0;
+    for (i = 0; i < pocet; i++)
+    {
+        if (pole[i] < 0) return -1;
+    }
 
-    for (i = 0; i < pocet; i++) {
-        if (pole[i] < 0) return -1; // zakazane hodnoty
-        if (pole[i] < pole[minIndex]) {
-            minIndex = i;
+    int min_index = 0;
+    for (i = 1; i < pocet; i++)
+    {
+        if (pole[i] < pole[min_index])
+        {
+            min_index = i;
         }
     }
-    return minIndex;
+    return min_index;
 }
 
-// maximalna hmotnost – vracia hodnotu
+// Funkcia vo vstupnom poli najde prvok s maximalnou hmotnostou
 int maximal(const int pole[], const int pocet)
 {
-    if (pole == 0 || pocet <= 0) return -1;
+    if (pocet <= 0 || pole == 0)
+        return -1;
 
     int i;
-    int max = pole[0];
-
-    for (i = 0; i < pocet; i++) {
+    for (i = 0; i < pocet; i++)
+    {
         if (pole[i] < 0) return -1;
-        if (pole[i] > max) {
+    }
+
+    int max = pole[0];
+    for (i = 1; i < pocet; i++)
+    {
+        if (pole[i] > max)
+        {
             max = pole[i];
         }
     }
     return max;
 }
 
-// sucet hmotnosti
+// Funkcia zisti celkovu hmotnost predmetov v krabici
 int summary(const int pole[], const int pocet)
 {
-    if (pole == 0 || pocet <= 0) return -1;
+    if (pocet <= 0 || pole == 0)
+        return -1;
 
     int i;
     int sum = 0;
 
-    for (i = 0; i < pocet; i++) {
+    for (i = 0; i < pocet; i++)
+    {
         if (pole[i] < 0) return -1;
         sum += pole[i];
     }
     return sum;
 }
 
-// priemer hmotnosti
+// Funkcia vypocita priemernu hmotnost predmetov v krabici
 double average(const int pole[], const int pocet)
 {
-    if (pole == 0 || pocet <= 0) return -1.0;
+    if (pocet <= 0 || pole == 0)
+        return -1.0;
 
     int i;
-    int sum = 0;
-
-    for (i = 0; i < pocet; i++) {
+    for (i = 0; i < pocet; i++)
+    {
         if (pole[i] < 0) return -1.0;
-        sum += pole[i];
     }
+
+    int sum = summary(pole, pocet);
+    if (sum == -1)
+        return -1.0;
 
     return (double)sum / pocet;
 }
-
